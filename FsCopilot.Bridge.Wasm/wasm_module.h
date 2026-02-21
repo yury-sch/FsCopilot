1151a0048844306eace117aad1e5f14498ce7d60
Yury Scherbakov <yurysch@icloud.com>
2026-02-21 18:54:09 +0300

    reduced the interpolation time

--- FsCopilot.Bridge.Wasm/wasm_module.h
+++ FsCopilot.Bridge.Wasm/wasm_module.h
@@ -7,6 +7,6 @@
 #include "stream_buffer.h"
 #include "time_shift.h"
 
-constexpr double k_delay_sec        = 0.8;
+constexpr double k_delay_sec        = 0.4;
 constexpr double k_max_age_sec      = 3.0;
 constexpr double k_offset_smoothing = 0.01;
